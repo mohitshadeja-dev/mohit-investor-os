a8d43dab41fd2786bc1416ae26f2375bb512133e
mohitshadeja-dev <mohit.shadeja@gmail.com>
2026-09-16 11:47:01 +0530

    Inject reverse-only weekly UI script

--- inject-lab.py
+++ inject-lab.py
@@ -4,7 +4,8 @@ s=p.read_text(encoding='utf-8')
 for tag in [
     '<script src="/static/lab-upgrade.js"></script>',
     '<script src="/static/presets-7750.js"></script>',
-    '<script src="/static/weekly-wma-gann.js"></script>'
+    '<script src="/static/weekly-wma-gann.js"></script>',
+    '<script src="/static/weekly-reverse-only.js"></script>'
 ]:
     if tag not in s:
         s=s.replace('</body>',tag+'\n</body>')
